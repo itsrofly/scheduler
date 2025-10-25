ffb0810fccaad597c75d3356d16fad422f9ae2c5
itsrofly <99050170+itsrofly@users.noreply.github.com>
2025-10-25 17:03:10 +0100

    fix: maxRetries must be null

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -7,7 +7,9 @@ import { Control } from '../../utils/control';
 const REDIS_URL = process.env.REDIS_URL;
 if (!REDIS_URL) throw new Error('REDIS_URL is not defined');
 
-const conn = new Redis(REDIS_URL);
+const conn = new Redis(REDIS_URL, {
+  maxRetriesPerRequest: null,
+});
 
 declare module 'fastify' {
   interface FastifyRequest {
